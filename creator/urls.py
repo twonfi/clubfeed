fa4a6c4fa8e8d87e81505d8ee901668ea519be21
twonfi <127453854+twonfi@users.noreply.github.com>
2025-05-29 00:54:31 -0700

    nav URLs and login status checks

--- creator/urls.py
+++ creator/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from creator.views import create_post
 
+app_name = 'creator'
+
 urlpatterns = [
     path('post/', create_post, name='create_post'),
 ]
--- viewer/urls.py
+++ viewer/urls.py
@@ -11,5 +11,5 @@ club_slug_patterns = [
 
 urlpatterns = [
     path('<slug:club_slug>/', include(club_slug_patterns)),
-    path('', club_list, name='club_post_list'),
+    path('', club_list, name='club_list'),
 ]
