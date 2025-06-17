c3b583a7770905fd874ccca8799b13ff0d50b0c1
twonfi <two@twonum.org>
2025-06-16 20:20:51 -0700

    temporarily remove pymdownx
    welcome to dependency hell...

--- core/templatetags/markdown_extras.py
+++ core/templatetags/markdown_extras.py
@@ -1,7 +1,10 @@
 from django import template
 from markdown import markdown as md
 from nh3 import clean
-from pymdownx.emoji import EmojiExtension, gemoji
+# from pymdownx.emoji import EmojiExtension, gemoji
+
+# pymdownx temporarily disabled due to a dependency hell situation with
+# Markdown versions conflicting with martor
 
 register = template.Library()
 
@@ -11,13 +14,14 @@ def sanitized_markdown(text):
     return md(clean(text), extensions=[
         'markdown.extensions.fenced_code',
         'markdown.extensions.tables',
-        'pymdownx.highlight',
-        'pymdownx.magiclink',
-        'pymdownx.betterem',
-        'pymdownx.tilde',
-        'pymdownx.tasklist',
-        'pymdownx.superfences',
-        'pymdownx.saneheaders',
-        'pymdownx.magiclink',
-        EmojiExtension(emoji_index=gemoji),
+        # 'pymdownx.highlight',
+        'codehilite'  # Temporary replacement
+        # 'pymdownx.magiclink',
+        # 'pymdownx.betterem',
+        # 'pymdownx.tilde',
+        # 'pymdownx.tasklist',
+        # 'pymdownx.superfences',
+        # 'pymdownx.saneheaders',
+        # 'pymdownx.magiclink',
+        # EmojiExtension(emoji_index=gemoji),
     ])
