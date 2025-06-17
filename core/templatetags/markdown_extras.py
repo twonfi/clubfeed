d8db092f885e72816d681768b4decfc5190e8da2
twonfi <two@twonum.org>
2025-06-16 20:13:03 -0700

    add Markdown extensions
    - syntax highlighting
    - emoji
    - other PyMdown Extensions (pymdownx) stuff

--- core/templatetags/markdown_extras.py
+++ core/templatetags/markdown_extras.py
@@ -1,15 +1,23 @@
 from django import template
 from markdown import markdown as md
 from nh3 import clean
+from pymdownx.emoji import EmojiExtension, gemoji
 
 register = template.Library()
 
 
 @register.filter
 def sanitized_markdown(text):
-    return clean(
-        md(text, extensions=[
-            "markdown.extensions.fenced_code",
-            "markdown.extensions.tables"
-        ])
-    )
+    return md(clean(text), extensions=[
+        'markdown.extensions.fenced_code',
+        'markdown.extensions.tables',
+        'pymdownx.highlight',
+        'pymdownx.magiclink',
+        'pymdownx.betterem',
+        'pymdownx.tilde',
+        'pymdownx.tasklist',
+        'pymdownx.superfences',
+        'pymdownx.saneheaders',
+        'pymdownx.magiclink',
+        EmojiExtension(emoji_index=gemoji),
+    ])
