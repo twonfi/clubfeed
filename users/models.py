8866664cd67e927282685decc0ab7b9ceaa4e777
twonfi <two@twonum.org>
2025-07-03 15:35:44 -0700

    add user profile model

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    """User profile.
+
+    This is used to store additional information for users that cannot
+     be stored in the base user object.
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+        primary_key=True)
+    picture = models.ImageField()
+    # description = models.TextField()
