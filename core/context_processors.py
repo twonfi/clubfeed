23ce6df93e3c908ccae0bf0d96ec4ad2ed7c074d
twonfi <two@twonum.org>
2025-07-03 17:12:35 -0700

    fix using `or` when `and` should be used

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,12 +2,13 @@ from django.db.models import Q
 
 from core.models import Club
 
+
 def can_post_context(request):
-    if (not request.user.is_authenticated
-            or not request.user.has_perm('core.add_post')
-            or not Club.objects.filter(
-                Q(owners=request.user) | Q(posters=request.user))):
+    if not request.user.is_authenticated or (
+        not request.user.has_perm("core.add_post")
+        and not Club.objects.filter(Q(owners=request.user) | Q(posters=request.user))
+    ):
         status = False
     else:
         status = True
-    return {'can_post': status}
+    return {"can_post": status}
